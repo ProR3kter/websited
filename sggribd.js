ed61a361d65a07456e0f09cd58fc02da6daa271f
R3kter <117512861+ProR3kter@users.noreply.github.com>
2025-10-09 11:49:32 +0200

    Update sggribd.js

--- sggribd.js
+++ sggribd.js
@@ -1,11 +1,18 @@
-var fff=document.getElementById('b')
-var m{"<img src='","' id='b'>"}
-var images={"https://i.pinimg.com/736x/42/14/c1/4214c1c2a3552a032972ff4ecb46144a.jpg","https://i.pinimg.com/736x/7b/d7/6c/7bd76c070b5b685f2ce78256dce76bc1.jpg","https://i.pinimg.com/736x/03/51/8a/03518a6e7c435e3e3a2ff7a799649b66.jpg"}
-var i = 0
+var fff = document.getElementById('b');
+var images = [
+  "https://i.pinimg.com/736x/42/14/c1/4214c1c2a3552a032972ff4ecb46144a.jpg",
+  "https://i.pinimg.com/736x/7b/d7/6c/7bd76c070b5b685f2ce78256dce76bc1.jpg",
+  "https://i.pinimg.com/736x/03/51/8a/03518a6e7c435e3e3a2ff7a799649b66.jpg"
+];
+var i = 0;
 
-while true {
-  setTimeout(function() {
-    fff.outerHTML=m[0] + images[i] + m[1];
-}, 2000);
+setInterval(function() {
+  if (!fff) {
+    fff = document.getElementById('b'); // In case it changes
+  }
+  fff.outerHTML = `<img src='${images[i]}' id='b'>`;
   i++;
-}
+  if (i >= images.length) {
+    i = 0;
+  }
+}, 2000);
